36f7b9c714badae0c5fc0000361a719bc3209702
Samuel Pierre <samuel.pierre@maibornwolff.de>
2021-03-11 11:14:59 +0100

    Removing redundant code

--- react/features/room-background/functions.js
+++ react/features/room-background/functions.js
@@ -23,31 +23,3 @@ export function extractBackgroundProperties(serializedBackgroundProperties) {
         backgroundImageUrl: unparsedBackgroundData[2]
     };
 }
-
-/**
- * The function synchronizes the information between remote participants to make sure to get the latest
- * background defined.
- *
- * @param {Object} participantsState - Participants redux state.
- * @param {Object} conferenceState - Conference redux state.
- * @private
- * @returns {Object}
- */
-export function getLatestBackground(participantsState, conferenceState) {
-
-    const remoteParticipants = participantsState
-        .filter(participant => !participant.local)
-        .map(p => (conferenceState?.participants || {})[p.id])
-        .map(p => extractBackgroundProperties(p?._properties?.backgroundData));
-
-    const participants = remoteParticipants
-        .filter(participant => participant.backgroundLastUpdate !== undefined);
-
-    const reference = participants
-        .sort((a, b) => parseInt(b.backgroundLastUpdate, 10) - parseInt(a.backgroundLastUpdate, 10))[0];
-
-    return {
-        backgroundColor: reference?.backgroundColor,
-        backgroundImageUrl: reference?.backgroundImageUrl
-    };
-}
