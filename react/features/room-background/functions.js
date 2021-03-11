45a34f0b4e09216488a50cc7bba4cac404c838a8
Samuel Pierre <samuel.pierre@maibornwolff.de>
2021-03-11 17:33:28 +0100

    Adding the possibility to adjust the size of the video is a background is set.

--- react/features/room-background/functions.js
+++ react/features/room-background/functions.js
@@ -4,7 +4,6 @@
  * Extract background-relevant information if existing from serialized background properties.
  *
  * @param {Object} serializedBackgroundProperties - Serialized background properties ('|' separated).
- * @private
  * @returns {Object}
  */
 export function extractBackgroundProperties(serializedBackgroundProperties) {
@@ -20,4 +19,20 @@ export function extractBackgroundProperties(serializedBackgroundProperties) {
         backgroundColor: unparsedBackgroundData[0],
         backgroundImageUrl: unparsedBackgroundData[1]
     };
+}
+
+/**
+ * Returns a boolean describing whether a background is currently set or not.
+ *
+ * @param {Object} state - Redux state.
+ * @returns {boolean}
+ */
+export function isRoomBackgroundDefined(state: Object = {}) {
+    const roomBackgroundState = state['features/room-background'];
+
+    if (roomBackgroundState?.backgroundImageUrl || roomBackgroundState?.backgroundColor) {
+        return true;
+    }
+
+    return false;
 }
