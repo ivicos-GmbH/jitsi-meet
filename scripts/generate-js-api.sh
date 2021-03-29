a4464e6dbe3fc8632dae54f59676c8f78d7d170a
Samuel Pierre <samuel.pierre@maibornwolff.de>
2021-03-29 16:26:59 +0200

    Adding changelog

--- scripts/generate-js-api.sh
+++ scripts/generate-js-api.sh
@@ -4,7 +4,7 @@ set -e
 echo 'Compiling external js file'
 make compile
 
-echo 'Coyping the compiled js file to the artifacts repository'
+echo 'Copying the compiled js file to the artifacts folder'
 mkdir -p artifacts/
 cp build/* artifacts
 
