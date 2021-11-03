8dc4f3423e8cc74099421818fb9a6f048fc88761
LancharroDav <davidglancharro@gmail.com>
2021-11-03 14:58:42 +0100

    fixed lint speaker-stats

--- react/features/speaker-stats/actionTypes.js
+++ react/features/speaker-stats/actionTypes.js
@@ -37,3 +37,8 @@ export const UPDATE_STATS = 'UPDATE_STATS';
  * }
  */
 export const INIT_REORDER_STATS = 'INIT_REORDER_STATS';
+
+/**
+ * Action used to configure the speaker stats related events
+ */
+export const CONFIGURE_SPEAKER_STATS_COLLECT = 'CONFIGURE_SPEAKER_STATS_COLLECT';
